51514eb389a0f91e29225aa6b11a9de7bbf6a1d1
aswathitv1 <aswathitv1@gmail.com>
2024-06-10 11:02:37 +0530

    Lazy loading

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
 import Header from './components/molecules/Header'
@@ -8,6 +8,7 @@ import Error from './components/molecules/Error'
 import About from './components/molecules/About'
 import Contact from './components/molecules/Contact'
 import RestCard from './components/atoms/RestCard'
+import Shimmer from './components/atoms/Shimmer'
 
 const AppLayout = () => {
     return(
@@ -18,6 +19,8 @@ const AppLayout = () => {
         </div>
     )
 }
+
+const Grocery = lazy(()=>import('./components/molecules/Grocery'))
 
 const appRouter = createBrowserRouter([
     {
@@ -36,6 +39,10 @@ const appRouter = createBrowserRouter([
             {
                 path:'/contact',
                 element:<Contact />
+            },
+            {
+                path:'/grocery',
+                element: <Suspense fallback={<Shimmer />}><Grocery /></Suspense>
             },
             {
                 path:'/restaurants/:id',
---
+++ components/molecules/Grocery.jsx
@@ -0,0 +1,7 @@
+const Grocery = () => {
+    return(
+        <h1>Grocery loaded with tons of other imaginary files</h1>
+    )
+}
+
+export default Grocery
