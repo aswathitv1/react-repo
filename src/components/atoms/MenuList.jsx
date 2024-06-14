06c1212d74c13ca9fea6cf05d24059cc248e032e
aswathitv1 <aswathitv1@gmail.com>
2024-06-14 12:37:48 +0530

    Check git

--- src/components/atoms/MenuList.jsx
+++ src/components/atoms/MenuList.jsx
@@ -1,6 +1,6 @@
 import { REST_IMG } from './../../utils/constants'
 const MenuList = ({list}) => {
-
+    console.log('check gitmerge main')
     return(
         <div>
             {list?.card?.card?.itemCards?.map(item=>
